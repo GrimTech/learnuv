76c57075aeb83934bd023d1300beba0bf781b319
Grim <olusijackson@gmail.com>
2026-03-11 22:08:38 +0000

    Mar 11, 2026, 11:08 PM

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -6,5 +6,5 @@ ROOT=$DIR/..
 cd $ROOT
 cp package.json package.orig.json                && \
   $DIR/bundle-dependencies.js $ROOT/package.json && \
-  npm pack                                       && \
+  bun pack                                       && \
   mv package.orig.json package.json 
